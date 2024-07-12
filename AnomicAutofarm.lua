e42637e4e1ed0935157b8d2a6fc3d6dbb7c18329
Sinduru <69154812+Sinduru@users.noreply.github.com>
2024-07-12 04:25:27 +0200

    Update AnomicAutofarm.lua

--- AnomicAutofarm.lua
+++ AnomicAutofarm.lua
@@ -4,7 +4,7 @@ genv.toggleState = false
 local function toggleAndPrint()
     genv.toggleState = not genv.toggleState
     if genv.toggleState then
-        print("RGIE IS AN ASSHOLE! NO MORE FUN 4 YOU!")
+        print("RGIE IS AN ASSHOLE! NO MORE FUN 4 YOU! GO SLEEP FOR THE NIGHT ZZZ")
     end
 end
 
